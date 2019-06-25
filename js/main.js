1f6a95cf1eca044eba4c4ff24680d37c288050ec
denmercs <denmercs@gmail.com>
2019-06-25 14:18:56 -0500

    working on css style (Goal: responsive design and animation)

---
+++ js/main.js
@@ -0,0 +1,68 @@
+//  OBJECTS
+
+const app = [{
+    name: 'HowTo',
+    tagLine: 'minimal. informative',
+    features: [
+        'Welcoming Interface for new users and registered users)', 
+        'Easy sign-up', 
+        'Easily add a how-to guide',
+        'Write a guide and choose when guide is published, otherwise it stays in draft.',
+        'Tag guides and filter through guides with keywords',
+        'Edit and delete post'],
+    goal: 'By alleviating the pain of lacking knowledge, we will give every individual on the planet the assets to learn how to do anything.',
+}, {
+    fontEnd: ['React', 'redux', 'logger', 'thunk', 'material-ui'],
+    backEnd: ['Express', 'knex', 'pg', 'jsonwebtoken', 'bcryptjs', 'jest', 'supertest']
+}];
+
+
+const team = [{
+    name: 'Leland Rogers',
+    role: 'Team Leader',
+    github: 'https://github.com/ltrii'
+}, 
+{
+    name: 'Isaiah Francois',
+    role: 'Web UI Developer I',
+    github: 'https://github.com/FrancoisCoding'
+},
+{
+    name: 'Levi Thomas',
+    role: 'Web UI Developer II',
+    github: 'https://github.com/TheLessonHere'
+},
+{
+    name: 'Dennis Mercado',
+    role: 'Web UI Developer III',
+    github: 'https://github.com/denmercs'
+},
+{
+    name: 'Andy Bettisworth',
+    role: 'Back End Developer',
+    github: 'https://github.com/wurde'
+},
+{
+    name: 'Jarred Stanford',
+    role: 'Front End Developer',
+    github: 'https://github.com/clem9281'
+}]
+
+
+
+
+
+// COMPONENTS
+
+
+
+
+
+
+
+
+
+
+
+
+// ANIMATIONS
